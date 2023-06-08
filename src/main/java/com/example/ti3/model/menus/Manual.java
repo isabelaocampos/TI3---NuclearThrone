65848326fac42ce03aae3d78d16750f34b5e5a0c
Valentina <valen201611@gmail.com>
2023-06-08 17:40:23 -0500

    ya no aguanto mas

---
+++ src/main/java/com/example/ti3/model/menus/Manual.java
@@ -0,0 +1,13 @@
+package com.example.ti3.model.menus;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+public class Manual {
+
+    @FXML
+    private ImageView keys;
+
+    @FXML
+    private ImageView takeAWeapon;
+}
