f3318f7a280b2c13b6ea62a4ea605eba449a976c
Valentina <valen201611@gmail.com>
2023-06-13 11:41:58 -0500

    Son las 11:41

--- src/main/java/com/example/nuclearthrone/model/menus/Soundtrack.java
+++ src/main/java/com/example/nuclearthrone/model/menus/Soundtrack.java
@@ -66,31 +66,10 @@ public class Soundtrack {
     public static Soundtrack getInstance() {
         if (instance == null) {
             instance = new Soundtrack();
-            initSounds();
+
         }
         return instance;
     }
 
-    public static void initSounds(){
-         isPlaying =new HashMap<>();
-         sounds = new HashMap<>();
-         sounds.put("walking_sound", new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/walking_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("footstep_grass",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/footstep_grass.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("03_before_the_dawn",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/03_before_the_dawn.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("goal_sound",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/goal_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("kickball_sound",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/kickball_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("playerDamage_sound",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/playerDamage_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("shot_by_slingshot",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/shot_by_slingshot.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("enemyKilled_sound",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/enemyKilled_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         sounds.put("gameOver_sound",new MediaPlayer(new Media("file:///" + new File("utils/soundtrack/gameOver_sound.mp3").getAbsolutePath().replace("\\", "/"))));
-         isPlaying.put("walking_sound",false);
-         isPlaying.put("03_before_the_dawn",false);
-         isPlaying.put("goal_sound",false);
-         isPlaying.put("footstep_grass",false);
-         isPlaying.put("kickball_sound",false);
-         isPlaying.put("playerDamage_sound",false);
-         isPlaying.put("shot_by_slingshot",false);
-         isPlaying.put("enemyKilled_sound",false);
-         isPlaying.put("gameOver_sound",false);
-    }
+
 }
